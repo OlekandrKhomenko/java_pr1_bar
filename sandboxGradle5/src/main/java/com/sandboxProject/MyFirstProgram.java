572aab44087d6cb9e3c7eded3c4fe9e04c996546
OleksandrKhomenko <sashakhomenko82@gmail.com>
2020-10-20 15:56:00 +0300

    Square autotest created

--- sandboxGradle5/src/main/java/com/sandboxProject/MyFirstProgram.java
+++ sandboxGradle5/src/main/java/com/sandboxProject/MyFirstProgram.java
@@ -1,7 +1,23 @@
 package com.sandboxProject;
 
-public class MyFirstProgram {
+public class MyFirstProgram<piblic> {
+
 	public static void main (String[] args){
-	System.out.println("Hello world");
+	//System.out.println("Hello world");
+    //home task function
+	Point p1 = new Point(2,5);
+	Point p2 = new Point(5,9);
+
+	System.out.println("the distance is = "+p2.checkDistance(p1,p2));
+
+	Square s = new Square(5);
+
+	Rectangle r = new Rectangle(5,8);
+
+		System.out.println("square of square with side a = "+s.l +" = "+ s.area());
+		System.out.println("square of rectangle with side a("+r.a+") and b("+r.b+") = "+r.area());
+
 	}
+
+
 }
