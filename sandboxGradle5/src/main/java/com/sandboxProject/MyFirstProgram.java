edc7f700f90de41cbc78ee2440c09f5bbb843069
OleksandrKhomenko <sashakhomenko82@gmail.com>
2020-11-12 18:26:53 +0200

    asdasd

--- sandboxGradle5/src/main/java/com/sandboxProject/MyFirstProgram.java
+++ sandboxGradle5/src/main/java/com/sandboxProject/MyFirstProgram.java
@@ -1,6 +1,6 @@
 package com.sandboxProject;
 
-public class MyFirstProgram<piblic> {
+public class MyFirstProgram {
 
 	public static void main (String[] args){
 	//System.out.println("Hello world");
--- sandboxGradle5/src/test/java/com/sandboxProject/SquareTests.java
+++ sandboxGradle5/src/test/java/com/sandboxProject/SquareTests.java
@@ -8,7 +8,7 @@ public class SquareTests {
     @Test
     public void testArea(){
         Square s = new Square(5);
-        //assert s.area()==25.0;
+        assert s.area()==25.0; // this variant doesn't count type of argument as double(work with integer)
         Assert.assertEquals(s.area(),25.0);
     }
 }
